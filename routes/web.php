c6e4ff1fa7b4d59c7f95ecceb6fa483eead00ad5
Shahrukh Ahmed <17292342-ShahrukhAhmed3@users.noreply.replit.com>
2026-04-03 14:29:33 +0000

    fixes

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::resource('/', ProjectController::class);
+Route::resource('projects', ProjectController::class);
